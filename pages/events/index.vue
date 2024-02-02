8aadf76aa84303392e3664644220e10d76c16041
ouhoy <75227707+ouhoy@users.noreply.github.com>
2024-02-02 10:25:25 +0100

    add layout

---
+++ pages/events/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>Events Page</div>
+<div>Events Page</div>
+<div>Events Page</div>
+<div>Events Page</div>
+</template>
+
+<style scoped>
+
+</style>
